d2442f07f49c28cb8aac8c3eb61b739a8dcda9ad
Ke_Jun <2454787839@qq.com>
2017-03-03 10:45:11 +0800

    主题大致完成

---
+++ about.md
@@ -0,0 +1,53 @@
+---
+layout: about
+title: About
+describe: about page
+---
+## 关于我
+
+KeJun 
+
+英文昵称的来源有点复杂。
+
+开始玩百度贴吧，惯用词`咳咳`（逃
+
+之后玩 Minecraft ，看到别人的昵称叫 `xxxJun` ，于是跟风。
+
+后来，所有平台的昵称都用：
+
+    KeJun
+    Ke_Jun
+    kejun
+    ke_jun
+    KeJun_Power
+    yrmkejun
+    kejunyrm
+
+总之，好气啊，太短了容易重复。
+
+---
+
+从事过：
+
+* 搬砖
+* 电焊
+* 商用厨具安装
+* 烤漆
+* 在线教育
+* 课件开发
+
+
+忠实谷粉（亲儿子为证）
+
+> 二次元智障 ——— KeJun
+
+---
+
+#### 联系我：
+
+| Name  |         Link         |
+|-------|:---------------------:|
+| QQ | 0A87536EC5DCBAEEE8416C88CD39CE8C    |
+| Mail |  kejun1997@gmail.com       |
+| Blog |  blog.kejun.tk       |
+
---
+++ js/main.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+  //scrollToTop
+  $("#toTop").scrollToTop();
+  // gallery
+  $('.gallery-expand').galleryExpand({
+    fillScreen: true,
+    onShow: function (el) {
+      // 沉浸状态栏
+      var defaultColor = $("nav.nav-extended").css("background-color");
+      var a = $("#placeholder-navbar").css("background-color");
+      $("meta[name='theme-color']").attr('content', a);
+      $("button.back-btn").click(function () {
+        $("meta[name='theme-color']").attr('content', defaultColor);
+      });
+      $("div#placeholder-overlay").click(function () {
+        $("meta[name='theme-color']").attr('content', defaultColor);
+      })
+      // code 高亮边框色
+      $("pre.highlight").css({
+        "border-left": "3px solid " + a,
+        "box-shadow": "-1px 0px 0px 0px " + a + ", 0px 0px 0px 1px " + a
+      });
+      //设置a标签的颜色
+      $("#postsContent a").css({
+        "color": a
+      })
+      //设置小代码块的颜色
+      $(".highlighter-rouge").css({
+        "color": a,
+        "border-radius": "0px"
+      })
+      // Carousel
+      var carousel = el.find('.carousel.initialized');
+      carousel.carousel({
+        dist: 0,
+        padding: 10
+      });
+      // Tabs
+      $('ul.tabs').tabs();
+    }
+  });
+  // 默认code边框颜色、返回顶部按钮，themecolor为第一个nav,
+  //获取颜色
+  var defaultColor = $("nav.nav-extended").css("background-color");
+  //设置代码框的颜色
+  $("pre.highlight").css({
+    "border-left": "3px solid " + defaultColor,
+    "box-shadow": "-1px 0px 0px 0px " + defaultColor + ", 0px 0px 0px 1px " + defaultColor
+  });
+  //设置blockquote的颜色
+  $("blockquote").css({
+    "border-left": "5px solid " + defaultColor,
+  });
+  //设置返回顶部的颜色
+  $("#toTop").css({
+    "background-color": defaultColor
+  })
+  //设置主题色
+  $("meta[name='theme-color']").attr('content', defaultColor);
+  //设置a标签的颜色
+  $("#postsContent a").css({
+    "color": defaultColor
+  })
+  //设置小代码块的颜色
+  $(".highlighter-rouge").css({
+    "color": defaultColor,
+    "border-radius": "0px"
+  })
+  //图片弹出
+  if($(".gallery-expand").css("background-color") == undefined){
+    $('#postsContent img').materialbox();
+  }
+});
